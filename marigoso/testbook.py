2b7135863c3b04c02b0594e07063327af0b1efeb
Ernesto D. Luzon Jr <edluzonjr@gmail.com>
2016-05-02 16:00:20 +0100

    improved reporting of traceback

--- marigoso/testbook.py
+++ marigoso/testbook.py
@@ -2,6 +2,7 @@ import pytest
 import nbformat
 import ntpath
 import re
+import pprint
 from queue import Empty
 from jupyter_client import KernelManager
 from marigoso import abstract
@@ -86,8 +87,9 @@ class TestScenario(pytest.File, abstract.Utils):
                 for step in ["### Given", "### And", "### When", "### Then", "### But"]:
                     if cell.source.startswith(step):
                         setup = False
-                        header = cell.source.split("\n")[0]
-                        header = self.delstring(header, ["### ", '(', ')', "'", '"'])
+                        self.header = cell.source.split("\n")[0].replace("### ", "")
+                        header = self.delstring(self.header, ["### ", '(', ')', "'", '"'])
+                        self.header = ".".join([self.name, self.header])
                         name = header.strip().replace(" ", "_").lower()
             if cell.cell_type == 'code' and nb.metadata.kernelspec.language == 'python':
                 if setup:
@@ -95,7 +97,7 @@ class TestScenario(pytest.File, abstract.Utils):
                     continue
                 if name == "Default Name":
                     continue
-                yield TestStep(name, self, cell)
+                yield TestStep(self.header, name, self, cell)
 
     def setup(self):
         self.km.restart_kernel()
@@ -105,51 +107,66 @@ class TestScenario(pytest.File, abstract.Utils):
                 option = "{} = '{}'".format(key, options[key])
                 self.kc.execute(option, allow_stdin=False)
         for setup in self.test_setup:
-            self.kc.execute(setup, allow_stdin=False)
+            send_and_execute(self.kc, setup, allow_stdin=False)
 
     def teardown(self):
         self.kc.execute("browser.quit()\n", allow_stdin=False)  # Quits any existing browser
         self.kc.stop_channels()
         self.km.shutdown_kernel(now=True)
 
-    def repr_failure(self, excinfo):
-        """ called when self.runtest() raises an exception. """
-        if isinstance(excinfo.value, TestException):
-            return "\n".join([
-                "TestItem execution failed",
-                "Source:\n%s\n\n"
-                "Traceback:\n%s\n" % excinfo.value.args,
-            ])
-        else:
-            return "pytest plugin exception: %s" % str(excinfo.value)
-
 
 class TestException(Exception):
     """ custom exception for error reporting. """
 
 
+def send_and_execute(kernel, source, item=None, allow_stdin=False):
+    run_id = kernel.execute(source, allow_stdin=allow_stdin)
+    timeout = 1800  # 1800seconds == 30minutes
+    while True:
+        try:
+            reply = kernel.get_shell_msg(block=True, timeout=timeout)
+            if reply.get("parent_header", None) and reply["parent_header"].get("msg_id", None) == run_id:
+                break
+        except Empty:
+            raise TestException("Timeout of %d seconds exceeded executing cell: %s"(timeout, source))
+
+    if reply['content']['status'] == 'ok':
+        return "Test successfully completed."
+
+    elif reply['content']['status'] == 'error':
+        _traceback = reply['content']['traceback']
+        if isinstance(item, TestStep):
+            item._location = (item._location[0], item._location[1], item.header)
+            if item.config.getvalue("color") == 'yes':
+                setattr(item, 'traceback', "\n".join(_traceback))
+            else:
+                ansi_escape = re.compile(r'\x1b[^m]*m')
+                setattr(item, 'traceback', ansi_escape.sub('', "\n".join(_traceback)))
+        raise TestException(source, _traceback)
+
+    elif reply['content']['status'] == 'aborted':
+        raise TestException(source, "Test was aborted")
+    else:
+        pprint.pprint(reply)
+        pprint.pprint(reply['content'])
+        raise TestException(source, "Unknown Status Code")
+
+
 class TestStep(pytest.Item, abstract.Utils):
 
-    def __init__(self, name, parent, cell):
+    def __init__(self, header, name, parent, cell):
         super(TestStep, self).__init__(name, parent)
+        self.header = header
         self.cell = cell
 
     def runtest(self):
-        run_id = self.parent.kc.execute(self.cell.source, allow_stdin=False)
-        timeout = 1800 #1800seconds == 30minutes
-        while True:
-            try:
-                reply = self.parent.kc.get_shell_msg(block=True, timeout=timeout)
-                if reply.get("parent_header", None) and reply["parent_header"].get("msg_id", None) == run_id:
-                    break
-            except Empty:
-                raise TestException("Timeout of %d seconds exceeded executing cell: %s" (timeout, self.cell.source))
+        send_and_execute(self.parent.kc, self.cell.source, item=self, allow_stdin = False)
 
-        if reply['content']['status'] == 'error':
-            _traceback = reply['content']['traceback']
-            if self.config.getvalue("color") == 'yes':
-                setattr(self, 'traceback', _traceback)
-            else:
-                ansi_escape = re.compile(r'\x1b[^m]*m')
-                setattr(self, 'traceback', ansi_escape.sub('', "\n".join(_traceback)))
-            raise TestException(self.cell.source, _traceback)
+    def repr_failure(self, excinfo):
+        if isinstance(excinfo.value, TestException):
+            try:
+                return ("\n\n".join([self.cell.source, self.traceback]))
+            except AttributeError:
+                return ("\n\n".join([self.cell.source, "\n".join(excinfo.value.args)]))
+        else:
+            return super(TestStep, self).repr_failure(excinfo)
